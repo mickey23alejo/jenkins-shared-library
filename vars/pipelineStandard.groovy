7f54128a266b180b975533eb104487ad003dc40f
mickey23alejo <mickey23alejo@gmail.com>
2020-12-15 12:30:23 -0500

    Update pipelineStandard.groovy

--- vars/pipelineStandard.groovy
+++ vars/pipelineStandard.groovy
@@ -3,7 +3,6 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    def label_name = config.agent?: 'Sebastian'
     pipeline{
         agent any
         stages{
