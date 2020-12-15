af5e67a082775ebdd13a5448bbab52e037876166
mickey23alejo <mickey23alejo@gmail.com>
2020-12-14 23:12:36 -0500

    Update Constants.groovy

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -1,6 +1,6 @@
 package org.example
 
 class Constants {
-  public static final String SLACK_MESSAGE = "Sending Slack Notification..."
-  public static final String EMAIL_MESSAGE = "Sending Email..."
+  public static final String SLACK_MESSAGE = "Sending Slack Notification... ALejo"
+  public static final String EMAIL_MESSAGE = "Sending Email... ALejo"
 }
