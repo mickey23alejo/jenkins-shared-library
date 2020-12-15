ede8e3a6e238b8a79405e3a4333539bd58604b5b
mickey23alejo <mickey23alejo@gmail.com>
2020-12-15 15:42:08 -0500

    Update pipelineStandard1.groovy

--- vars/pipelineStandard1.groovy
+++ vars/pipelineStandard1.groovy
@@ -1,1 +1,31 @@
-
+def call(body) {
+    def config= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    pipeline{
+        agent any
+        stages{
+            stage('Build alejandro') {
+                steps {
+                     script {
+                        def browsers = ['chrome', 'firefox']
+                        for (int i = 0; i < browsers.size(); ++i) {
+                            echo "probando el browser ${browsers[i]}."
+                        }
+                     }
+                }
+            }
+            stage('Test: Alejandro') {
+                steps {
+                     echo 'Buiding alejandro'
+                }
+            }
+            stage('Sorbinos') {
+                steps {
+                    echo 'Buiding sobrinos'
+                }
+            }
+        }
+    }
+}
