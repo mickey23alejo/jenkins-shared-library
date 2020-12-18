caed1efe510c3d36614fe64565353909a760cebc
mickey23alejo <mickey23alejo@gmail.com>
2020-12-17 23:48:33 -0500

    prueba

--- vars/pipelineStandard1.groovy
+++ vars/pipelineStandard1.groovy
@@ -5,10 +5,6 @@ def call(body) {
     body()
     pipeline{
         agent any
-        environment {
-            CRED_USUARIO = credentials('USUARIO_ALEJO')
-            SECRET_TEXT = credentials('USUARIO_ALEJO')
-        }
         stages{
             stage('Build alejandro') {
                 steps {
@@ -23,15 +19,6 @@ def call(body) {
             stage('Sorbinos') {
                 steps {
                     echo 'Buiding sobrinos'
-                }
-            }
-            stage('Variables y texto ocualto') {
-                steps {
-                    sh '''
-                    echo "El usuario es $CRED_USUARIO_USR
-                    echo "La contraseña es $CRED_USUARIO_PSW"'
-                    echo "La contrasenia es $SECRET_TEXT"
-                    '''
                 }
             }
         }
