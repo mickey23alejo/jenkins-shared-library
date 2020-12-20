ff082b1c31376e8cf78a199579a4efac6540860f
mickey23alejo <mickey23alejo@gmail.com>
2020-12-20 14:08:27 -0500

    prueba

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -21,6 +21,11 @@ def call(body) {
                 
             }        
           }
+          stage('Push to Nexus'){
+            steps{
+              echo "$NEXUS_PASSWORD"
+            }
+          }
           /*stage('Conexion ssh') {
             steps{
               //sh 'ssh root@192.3.50.101 "kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/qa-cadcargamasiva-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true"'
