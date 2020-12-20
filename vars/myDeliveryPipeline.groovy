3e560757273ced27a365a2e76e0e91a992a24ce8
mickey23alejo <mickey23alejo@gmail.com>
2020-12-20 13:56:57 -0500

    prueba

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -6,12 +6,12 @@ def call(body) {
     body()
 
     pipeline {
-        agent any
+        agent { label 'first_slave' }
         stages {
           stage('Docker build') {
             steps {
                 sh 'cd $WORKSPACE'
-                //sh "/usr/bin/docker build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
+                sh "/usr/bin/docker build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
                 //sh "mkdir '${config.name}'"
                 /*sh '''
                 cd $WORKSPACE
@@ -21,12 +21,12 @@ def call(body) {
                 
             }        
           }
-          stage('Conexion ssh') {
+          /*stage('Conexion ssh') {
             steps{
               //sh 'ssh root@192.3.50.101 "kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/qa-cadcargamasiva-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true"'
               sh "ssh root@192.3.50.101 'kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/${config.name}-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true'"
             }
-          } 
+          } */
         }    
     }
 }
