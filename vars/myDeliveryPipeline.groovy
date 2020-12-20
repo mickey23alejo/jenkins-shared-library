3586da61991829808e7e5617d9d49f5843f5e85d
mickey23alejo <mickey23alejo@gmail.com>
2020-12-20 16:18:51 -0500

    prueba varaibles

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -47,7 +47,7 @@ def call(body) {
             steps{
               //sh 'ssh root@192.3.50.101 "kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/qa-cadcargamasiva-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true"'
               //sh "ssh root@192.3.50.101 'kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/${config.name}-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true'"
-              sh "ssh root@192.3.50.101 'kubectl apply -f ${config.urlYaml} && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/qa-${config.name}-image:v1.0.$BUILD_NUMBER -n kube-${config.namespace}  --record=true'"
+              sh "ssh root@192.3.50.101 'kubectl apply -f ${config.urlYaml} && kubectl set image deployment.v1.apps/${config.name}-deploy ${config.name}=192.3.50.170:8083/qa-${config.name}-image:v1.0.$BUILD_NUMBER -n kube-${config.namespace}  --record=true'"
             }
           }
         }    
