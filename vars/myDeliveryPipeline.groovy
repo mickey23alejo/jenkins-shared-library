d23ac6a73c0d4a0a24e152e6a738f5bff3a42215
mickey23alejo <mickey23alejo@gmail.com>
2020-12-20 02:20:17 -0500

    mmmmmmmmmmmmm

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -11,7 +11,7 @@ def call(body) {
           stage('Primer paso') {
             steps {
                 sh 'cd $WORKSPACE'
-                sh "/usr/bin/docker build -f Dockerfile -t '${config.name}'-image:v1.0.$BUILD_NUMBER ."
+                //sh "/usr/bin/docker build -f Dockerfile -t '${config.name}'-image:v1.0.$BUILD_NUMBER ."
                 //sh "mkdir '${config.name}'"
                 /*sh '''
                 cd $WORKSPACE
@@ -21,11 +21,11 @@ def call(body) {
                 
             }        
           }
-          /*stage('Conexion ssh') {
+          stage('Conexion ssh') {
             steps{
-              //sh 'ssh root@192.3.50.101 "kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/"-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true"'
+              sh 'ssh root@192.3.50.101 "kubectl apply -f http://192.3.50.170:8081/repository/maven-releases/pryqa/cadcargamasiva/v1/cadcargamasiva-v1-kubernetes.yaml && kubectl set image deployment.v1.apps/cadcargamasiva-deploy cadcargamasiva=192.3.50.170:8083/qa-cadcargamasiva-image:v1.0.$BUILD_NUMBER -n kube-prynep01  --record=true"'
             }
-          } */     
+          } 
         }    
     }
 }
