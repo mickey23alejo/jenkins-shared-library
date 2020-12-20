452302b4055b0cc612521c71c08faafec2387e17
mickey23alejo <mickey23alejo@gmail.com>
2020-12-20 13:14:39 -0500

    mmmmmmmmmmmm

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -10,8 +10,8 @@ def call(body) {
         stages {
           stage('Primer paso') {
             steps {
-                sh 'cd $WORKSPACE'
-                //sh "/usr/bin/docker build -f Dockerfile -t '${config.name}'-image:v1.0.$BUILD_NUMBER ."
+                //sh 'cd $WORKSPACE'
+                sh "/usr/bin/docker build -f Dockerfile -t '${config.name}'-image:v1.0.$BUILD_NUMBER ."
                 //sh "mkdir '${config.name}'"
                 /*sh '''
                 cd $WORKSPACE
